61bc3fd8bfe1c93a2bf035eeb708cf23679b87af
Yota Ichino <www.irda@gmail.com>
2019-09-09 23:28:03 +0900

    Log with thread ID

--- src/log.h
+++ src/log.h
@@ -1,4 +1,6 @@
 #pragma once
 
-#define LOG_DEBUG(FORMAT, ...) fprintf(stderr, "LOG_DEBUG " FORMAT "\n", ## __VA_ARGS__)
-#define LOG_ERROR(FORMAT, ...) fprintf(stderr, "LOG_ERROR " FORMAT "\n", ## __VA_ARGS__)
+#include <uv.h>
+
+#define LOG_DEBUG(FORMAT, ...) fprintf(stderr, "LOG_DEBUG %p " FORMAT "\n", uv_thread_self(), ## __VA_ARGS__)
+#define LOG_ERROR(FORMAT, ...) fprintf(stderr, "LOG_ERROR %p " FORMAT "\n", uv_thread_self(), ## __VA_ARGS__)
