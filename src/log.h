b77e8a9b289f12e25615072e1c9cb38cd995d347
Yota Ichino <www.irda@gmail.com>
2019-10-14 21:35:46 +0900

    Add LOG_INFO

--- src/log.h
+++ src/log.h
@@ -16,5 +16,7 @@ extern LOG_LEVEL_E log_level;
 
 void set_log_level(LOG_LEVEL_E level);
 
+#define LOG_ERROR(FORMAT, ...) do { if (log_level >= LOG_LEVEL_ERROR) fprintf(stderr, "LOG_ERROR " FORMAT "\n", ## __VA_ARGS__); } while(0)
+#define LOG_INFO(FORMAT,  ...) do { if (log_level >= LOG_LEVEL_INFO)  fprintf(stderr, "LOG_INFO  " FORMAT "\n", ## __VA_ARGS__); } while(0)
 #define LOG_DEBUG(FORMAT, ...) do { if (log_level >= LOG_LEVEL_DEBUG) fprintf(stderr, "LOG_DEBUG " FORMAT "\n", ## __VA_ARGS__); } while(0)
 void LOG_CONNECTION_ID(LOG_LEVEL_E level, const std::vector<uint8_t> &cid);
