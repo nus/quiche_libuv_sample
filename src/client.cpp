b9aec7106b3510bbbe4d4198b65d4758c1f704df
Yota Ichino <www.irda@gmail.com>
2019-10-16 22:07:57 +0900

    Send file by quic

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,8 @@
 #include "log.h"
 #include "quic_client.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <thread>
@@ -12,22 +14,56 @@ void sleep_for(ssize_t millis) {
     std::this_thread::sleep_for(std::chrono::milliseconds(millis));
 }
 
-void set_dummy_data(uint8_t *data, size_t len) {
-    for (int i = 0; i < len; i++) {
-        data[i] = '0' + (i % 10);
+static off_t read_file(const char *path, char **buffer) {
+    int fd = -1;
+    FILE *fp = 0;
+    struct stat stbuf;
+    char *buf = NULL;
+
+    if ((fd = open(path, O_RDONLY)) == 1) {
+        fprintf(stderr, "open(%s) failed: %d\n", path, errno);
+        goto err;
+    } else if (fstat(fd, &stbuf) == -1) {
+        fprintf(stderr, "fstat() failed: %d\n", errno);
+        goto err;
+    } else if (!(fp = fdopen(fd, "r"))) {
+        fprintf(stderr, "fdopen() failed: %d\n", errno);
+        goto err;
+    } else if (!(buf = (char *) calloc(1, stbuf.st_size))) {
+        fprintf(stderr, "calloc() failed.\n");
+        goto err;
+    } else if ((fread(buf, sizeof(char), stbuf.st_size, fp)) != stbuf.st_size) {
+        fprintf(stderr, "fread() failed.\n");
+        goto err;
+    } else {
+        *buffer = buf;
+        return stbuf.st_size;
     }
+
+err:
+    if (buf) free(buf);
+    if (fp) fclose(fp);
+    if (fd == -1) close(fd);
+
+    return -1;
 }
 
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        LOG_ERROR("Set arguments: %s <host> <port>", argv[0]);
+    ssize_t buf_len;
+    char *buf;
+
+    if (argc < 4) {
+        LOG_ERROR("Set arguments: %s <host> <port> <file_to_upload>", argv[0]);
+        return -1;
+    } else if ((buf_len = read_file(argv[3], &buf)) < 0) {
+        LOG_ERROR("read_file(%s) failed", argv[3]);
         return -1;
     }
+    
     QuicClient *client = new QuicClient(argv[1], argv[2]);
     equic_client_t e;
-
     for (e = client->connect(); e == EQUIC_CLIENT_AGAIN; e = client->connect()) {
         sleep_for(30);
     }
@@ -39,20 +75,20 @@ int main(int argc, char *argv[]) {
     LOG_DEBUG("connected to host.");
 
     bool req_sent = false;
-    uint8_t r[100] = {0};
-    int count = 0;
-    set_dummy_data(r, sizeof(r));
-    int n = 0;
+    size_t payload_len = 1024;
+    int cur = 0;
     while ((e = client->progress_while_connected()) == EQUIC_CLIENT_AGAIN) {
         sleep_for(1);
 
         if (!req_sent) {
-            n += client->stream_send(4, r, sizeof(r), count > 100);
-            printf("n: %d\n", n);
-            if (count > 100) {
+            bool fin = (buf_len - cur) < payload_len;
+            int len = fin ? (buf_len - cur) : payload_len;
+            cur += client->stream_send(4, (uint8_t *) buf, len, fin);
+
+            if (fin) {
                 req_sent = true;
             }
-            count++;
+            printf("cur: %d\n", cur);
         } else {
             IQuicClientStreamIter *iter = client->readable();
             if (iter) {
